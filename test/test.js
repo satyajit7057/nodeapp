8f5b79323ba5d457a5996d43e4a7929dbec26b2a
satyajit7057 <satyajitdeshmukh371@gmail.com>
2023-04-27 18:29:52 +0530

    fourth

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var request = require('supertest');
 var app = require('../index.js');describe('GET /', function() {
  it('respond with hello world', function(done) { //navigate to root and check the the response is "hello world"
- request(app).get('/').expect('hello my world', done);
+ request(app).get('/').expect('hello beautiful world', done);
  });
 });
